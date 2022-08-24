6c0708ecf6d8b4614a00880251c566b21813d7b4
unknown <kimchoxd@gmail.com>
2022-08-24 16:57:34 -0600

    Last commit

--- src/hooks/deleteCar.js
+++ src/hooks/deleteCar.js
@@ -1,14 +1,11 @@
 export const deleteCar = (id) => {
     let list = []
     list = JSON.parse(window.sessionStorage.getItem("product"));
-    console.log('Entra')
-    console.log(id)
-    console.log('-------------------------------------')
     for(let i = 0 ; i < list.length ; i++){  
         if(list[i].id === id){
             list.splice(i,1)
         }
     }
     window.sessionStorage.setItem("product", JSON.stringify(list));
-    window.location.reload();
+    window.location.reload(true);
 }
